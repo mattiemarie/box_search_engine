871c28c179d690d4dd19d000ad79b48fd321437a
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-08 20:53:23 -0500

    adding to mutation.js

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: )
+    }`;
+
+export const ADD_USER = gql``;
+
+export const SAVE_BOOK = gql``;
+
+export const REMOVE_BOOK = gql``;
