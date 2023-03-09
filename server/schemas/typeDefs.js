1887a788c79bf8f966b5758fac04083ef492a37f
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-08 19:39:58 -0500

    adding to typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,27 @@ const typeDefs = gql `
        login(email: String!, password: String!): Auth
        addUser(username: String!, email: String!, password: String!): Auth
        saveBook(sss description: String!, title: String!, bookId: Id!, image: image, link: link): User
+       removeBook(bookId: Id!): User
     }
-
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: String
+        savedBooks: [bookSchema]
+    }
+    type Book {
+        bookID: ID
+        authors: String
+        description: String
+        title: String
+        image: image
+        link: link
+    }
+    type Auth {
+        token: ID!
+        user: user
+    }
 `;
+
+module.exports = typeDefs;
