5c8409095ec5f0f30b2c9960e7975bbb66da4656
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-09 18:02:19 -0500

    add new_user to mutation.js

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -11,9 +11,20 @@ export const LOGIN_USER = gql`
             }
         }
     }
-    `;
+`;
 
-export const ADD_USER = gql``;
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
 
 export const SAVE_BOOK = gql``;
 
