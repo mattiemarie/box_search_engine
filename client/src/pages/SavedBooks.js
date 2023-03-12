f0267a861c0d8959429ccc76b0b5ad3233c6f19c
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-12 12:44:46 -0400

    added to mutations.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,7 +9,7 @@ import {
 
 // import { getMe, deleteBook } from '../utils/API';
 import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutation';
+import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -12,6 +12,10 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+//added imports
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations'
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
