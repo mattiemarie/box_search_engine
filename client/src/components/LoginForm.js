1a8633bb2de94907b4b0a1598218a7f7e5effc57
Mattie Gonzalez <gonzalem@lssc.edu>
2023-03-13 01:22:29 -0400

    adding mutatioiin to loginform.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,19 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+//add imports
+import { userMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  //adding LOGIN_USER
+  const [login, {loading}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
